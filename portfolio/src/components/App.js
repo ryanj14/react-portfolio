e248077f07ff50a9bf7f5c090550772dbcb8dba0
Ryan <josephmryan14@gmail.com>
2019-01-25 14:50:53 -0800

    Added title component to project

--- portfolio/src/components/App.js
+++ portfolio/src/components/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import NavBar from './NavBar';
 import Title from './Title';
+import Skills from './Skills';
 
 const App = () => {
   return(
     <div>
       <NavBar/>
       <Title/>
+      <Skills/>
     </div>
   );
 };
