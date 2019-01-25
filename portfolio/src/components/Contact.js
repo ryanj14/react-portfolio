aa3cc1bfab3415a7c9c6fbaf19e29b8f989493ec
Ryan <josephmryan14@gmail.com>
2019-01-25 15:55:36 -0800

    updated to jsx

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -6,15 +6,15 @@ const Contact = () => {
     <section class="contact">
       <article>
         <h3>Contact</h3>
-        <div class="contact1">
+        <div className="contact1">
           <img src={require("../img/GitHub-Mark-64px.png")} alt="Github logo"></img>
           <a href="https://github.com/ryanj14">github.com/ryanj14</a>
         </div>
-        <div class="contact2">
+        <div className="contact2">
           <img src={require("../img/linkedin.png")} alt="Linkedin logo"></img>
           <a href="https://www.linkedin.com/in/ryan-joseph-226682163/">linkedin.com/in/ryan-joseph-226682163</a>
         </div>
-        <div class="contact3">
+        <div className="contact3">
           <img src={require("../img/email.png")} alt="Email"></img>
           <p>josephmryan14@gmail.com</p>
         </div>
