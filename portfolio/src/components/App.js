b2dc5699f646ce84191b37a442077a7f86b76fff
Ryan <josephmryan14@gmail.com>
2019-01-25 14:27:31 -0800

    Added images and jquery to project

--- portfolio/src/components/App.js
+++ portfolio/src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from 'react';
 import NavBar from './NavBar';
+import Title from './Title';
 
 const App = () => {
   return(
     <div>
       <NavBar/>
+      <Title/>
     </div>
   );
 };
--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -4,12 +4,12 @@ import '../styles/style.min.css'
 const NavBar = () => {
   return(
     <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
-      <button className="navbar-toggler" type="button">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <a className="navbar-brand" href=" ">Ryan Joseph</a>
 
-      <div className="collapse navbar-collapse">
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
           <li className="nav-item">
             <a className="nav-link" href="https://crowdvote.000webhostapp.com/">Crowd Vote</a>
