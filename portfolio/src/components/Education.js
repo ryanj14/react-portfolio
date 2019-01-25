265e5eadcc1892db6581e9765bb60e704dad48bd
Ryan <josephmryan14@gmail.com>
2019-01-25 15:56:41 -0800

    Updated to jsx

--- portfolio/src/components/Education.js
+++ portfolio/src/components/Education.js
@@ -3,10 +3,10 @@ import '../styles/style.min.css'
 
 const Education = () => {
   return(
-    <section class="education">
+    <section className="education">
       <article>
         <h3>Education</h3>
-        <div class="educationStory">
+        <div className="educationStory">
             <h6>British Columbia Institute of Technology <div>2017 - Present</div></h6>
             <p>Computer Systems Technology Diploma program </p>
             <p>Web and Mobile option</p>
