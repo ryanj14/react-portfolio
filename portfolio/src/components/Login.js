20dbb9b9e4092ed0fe2200abe6005d1713a76aad
Ryan <josephmryan14@gmail.com>
2019-02-09 14:39:56 -0800

    Connected login with redux

--- portfolio/src/components/Login.js
+++ portfolio/src/components/Login.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import { user, password } from '../actions';
+import { connect } from 'react-redux';
 
-const Login = () => {
-  return(
-    <form className="ui form">
-      <div className="field">
-        <label>User</label>
-        <input type="text" name="username" />
-      </div>
-      <div className="field">
-        <label>Password</label>
-        <input type="password" name="password" />
-      </div>
-      <button className="ui button" type="submit">Submit</button>
-    </form>
-  );
-};
+class Login extends React.Component {
 
-export default Login;
+  handleFormSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    console.log(this.props);
+    return(
+      <form className="ui form">
+        <div className="field">
+          <label>User</label>
+          <input 
+            type="text" 
+            name="username" 
+            onChange={ e => this.props.user(e.target.value) }
+          />
+        </div>
+        <div className="field">
+          <label>Password</label>
+          <input 
+            type="password" 
+            name="password" 
+            onChange={ e => this.props.password(e.target.value) }
+          />
+        </div>
+        <button 
+          className="ui button" 
+          type="submit"
+          onClick={ e => this.handleFormSubmit(e) }
+        >
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+export default connect(null, { user, password })(Login);
