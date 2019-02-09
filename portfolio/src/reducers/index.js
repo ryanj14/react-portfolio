d4cb6de1bd32836e916fd1bc26df1edab5cbfab8
Ryan <josephmryan14@gmail.com>
2019-02-09 14:29:46 -0800

    Added loginReducer function

--- portfolio/src/reducers/index.js
+++ portfolio/src/reducers/index.js
@@ -1,5 +1,25 @@
 import { combineReducers } from 'redux';
+import { 
+  USERNAME,
+  PASSWORD
+} from '../actions/types';
+
+const INITIAL_STATE = {
+  user: '',
+  password: ''
+};
+
+const loginReducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    case USERNAME:
+      return { ...state, user: action.payload };
+    case PASSWORD:
+      return { ...state, password: action.payload };
+    default:
+      return state;
+  }
+}
 
 export default combineReducers({
-  songs: 'whatever'
+  userLogin: loginReducer
 });
