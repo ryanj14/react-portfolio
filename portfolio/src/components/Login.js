a3d2b5205f1d963f4cfd10f08fe996988c034e09
Ryan <josephmryan14@gmail.com>
2019-02-09 14:05:13 -0800

    Changed it to user and passeword inputs

--- portfolio/src/components/Login.js
+++ portfolio/src/components/Login.js
@@ -4,18 +4,12 @@ const Login = () => {
   return(
     <form className="ui form">
       <div className="field">
-        <label>First Name</label>
-        <input type="text" name="first-name" placeholder="First Name" />
+        <label>User</label>
+        <input type="text" name="username" />
       </div>
       <div className="field">
-        <label>Last Name</label>
-        <input type="text" name="last-name" placeholder="Last Name" />
-      </div>
-      <div className="field">
-        <div className="ui checkbox">
-          <input type="checkbox" tabindex="0" class="hidden" />
-          <label>I agree to the Terms and Conditions</label>
-        </div>
+        <label>Password</label>
+        <input type="password" name="password" />
       </div>
       <button className="ui button" type="submit">Submit</button>
     </form>
