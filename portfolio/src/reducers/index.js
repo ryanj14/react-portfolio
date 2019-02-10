e48fafacbc5e5215362195d83d37fb4e91152a3a
Ryan <josephmryan14@gmail.com>
2019-02-10 13:55:50 -0800

    Connected it to the reducers

--- portfolio/src/reducers/index.js
+++ portfolio/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import loginReducer from './loginReducer';
 import blogReducer from './blogReducer';
+import apiReducer from './apiReducer';
 
 export default combineReducers({
   userLogin: loginReducer,
-  blog: blogReducer
+  blog: blogReducer,
+  api: apiReducer
 });
