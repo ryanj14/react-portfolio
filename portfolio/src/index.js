a7dbb955664b7445041180b59e067375ad904c45
Ryan <josephmryan14@gmail.com>
2019-02-13 09:47:55 -0800

    Added persistant state once user refreshs page

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -6,11 +6,19 @@ import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
+import { loadState, saveState } from './localStorage';
+
+const persistedState = loadState();
 
 const store = createStore(
   reducers,
+  persistedState,
   applyMiddleware(thunk)
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 ReactDOM.render(
   <Provider store={ store }>
