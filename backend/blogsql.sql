0e34093d9cbbd7a41451fa2bdf60d31acb96a94d
Ryan <josephmryan14@gmail.com>
2019-02-15 11:40:51 -0800

    Added sql file

---
+++ backend/blogsql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Blog
+(
+    id              INT(6)          NOT NULL UNSIGNED AUTO_INCREMENT PRIMARY KEY
+    ,title          VARCHAR(30)     NOT NULL
+    ,author         VARCHAR(255)    NOT NULL
+    ,body           TEXT            NOT NULL
+    ,blogDate       VARCHAR(255)    NOT NULL
+    ,blogId         INT(6)          NOT NULL
+);
