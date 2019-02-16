ad2794f00bef967e449058f6e9193d73d3ecb268
Ryan <josephmryan14@gmail.com>
2019-02-16 09:19:53 -0800

    Styled login page

--- portfolio/src/components/Login.js
+++ portfolio/src/components/Login.js
@@ -13,31 +13,41 @@ class Login extends React.Component {
 
   render() {
     return(
-      <form className="ui form">
-        <div className="field">
-          <label>User</label>
-          <input 
-            type="text" 
-            name="username" 
-            onChange={ e => this.props.user(e.target.value) }
-          />
-        </div>
-        <div className="field">
-          <label>Password</label>
-          <input 
-            type="password" 
-            name="password" 
-            onChange={ e => this.props.password(e.target.value) }
-          />
-        </div>
-        <button 
-          className="ui button" 
-          type="submit"
-          onClick={ e => this.handleFormSubmit(e) }
-        >
-          Submit
-        </button>
-      </form>
+      <div className="ui placeholder segment" style={{ margin: '5%'}}>
+        <form className="ui form">
+          <div className="field">
+            <label>User</label>
+            <div className="ui left icon input">
+              <input 
+                type="text" 
+                name="username" 
+                onChange={ e => this.props.user(e.target.value) }
+                placeholder="Username"
+              />
+              <i className="user icon"></i>
+            </div>
+          </div>
+          <div className="field">
+            <label>Password</label>
+            <div className="ui left icon input">
+              <input 
+                type="password" 
+                name="password" 
+                onChange={ e => this.props.password(e.target.value) }
+              />
+              <i class="lock icon"></i>
+            </div>
+          </div>
+          <button 
+            className="ui blue button" 
+            type="submit"
+            onClick={ e => this.handleFormSubmit(e) }
+            style ={{ width: '100%'}}
+          >
+            Login
+          </button>
+        </form>
+      </div>
     );
   }
 }
