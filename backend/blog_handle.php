db596ae61744e7d6dbef2f635a3e40060df6120e
Ryan <josephmryan14@gmail.com>
2019-02-17 15:22:55 -0800

    Using this to fix the cors issue

--- backend/blog_handle.php
+++ backend/blog_handle.php
@@ -1,9 +1,27 @@
 <?php
-  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-  header("Access-Control-Allow-Credentials: true");
-  header("Access-Control-Max-Age: 1000");
-  header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
-  header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE, PATCH");
+
+  // Allow from any origin
+  if (isset($_SERVER['HTTP_ORIGIN'])) {
+    // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+    // you want to allow, and if so:
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+  } else {
+    echo "Not working";
+    die();
+  }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+      if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+          // may also be using PUT, PATCH, HEAD etc
+          header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PATCH");         
+
+      if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+          header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    }
 
   require_once('db_info.php');
 
