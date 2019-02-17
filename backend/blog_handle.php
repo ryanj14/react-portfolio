a4e331f48f67900520853a442e9aa7114280abcd
Ryan <josephmryan14@gmail.com>
2019-02-17 15:04:21 -0800

    Changed it to work with heroku

--- backend/blog_handle.php
+++ backend/blog_handle.php
@@ -1,5 +1,5 @@
 <?php
-  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
   header("Access-Control-Allow-Credentials: true");
   header("Access-Control-Max-Age: 1000");
   header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
