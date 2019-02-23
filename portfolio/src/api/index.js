a696762a8f2da7f1638872984e882734c83bca59
Ryan <josephmryan14@gmail.com>
2019-02-23 11:51:22 -0800

    Removed redundant variable

--- portfolio/src/api/index.js
+++ portfolio/src/api/index.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-export const login = axios.create({
-  baseURL: 'http://localhost/react-portfolio/backend'
-});
-
 export const blogs = axios.create({
   baseURL: 'http://localhost/react-portfolio/backend',
 });
