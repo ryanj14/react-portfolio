d2991f3236a1561c26fdbaf58c8c8f5b6d61e91c
Ryan <josephmryan14@gmail.com>
2019-02-23 12:46:50 -0800

    Refactored code

--- backend/blog_handle.php
+++ backend/blog_handle.php
@@ -11,16 +11,14 @@
     die();
   }
 
-    // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-      if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-          // may also be using PUT, PATCH, HEAD etc
-          header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PATCH");         
-
-      if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-          header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    }
+  // Access-Control headers are received during OPTIONS requests
+  if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+      // may also be using PUT, PATCH, HEAD etc
+      header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PATCH");         
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+      header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+  }
 
   require_once('db_info.php');
 
@@ -32,23 +30,31 @@
   $body = $_POST['blog'];
   $date = $_POST['date'];
 
-  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+  function connect_to_db() {
+    try {
       $conn = new PDO("mysql:host=127.0.0.1; dbname=assign1", DB_USER, DB_PASSWORD);
       // set the PDO error mode to exception
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $stmt = $conn->prepare("INSERT INTO Blog (title, author, body, blogDate)
-              VALUES (?, ?, ?, ?)");
-      $stmt->bindParam(1, $title);
-      $stmt->bindParam(2, $author);
-      $stmt->bindParam(3, $body);
-      $stmt->bindParam(4, $date);
-      // use exec() because no results are returned
-      $stmt->execute();
-      echo "New record created successfully";
+      return $conn;
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+      die();
+    }
+  }
+
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $conn = connect_to_db();
+    $stmt = $conn->prepare("INSERT INTO Blog (title, author, body, blogDate)
+                            VALUES (?, ?, ?, ?)");
+    $stmt->bindParam(1, $title);
+    $stmt->bindParam(2, $author);
+    $stmt->bindParam(3, $body);
+    $stmt->bindParam(4, $date);
+    // use exec() because no results are returned
+    $stmt->execute();
+    echo "New record created successfully";
   } else if($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $conn = new PDO("mysql:host=127.0.0.1; dbname=assign1", DB_USER, DB_PASSWORD);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = connect_to_db();
     $sth = $conn->prepare("SELECT * FROM Blog");
     $sth->execute();
 
@@ -56,9 +62,7 @@
     $result = $sth->fetchAll();
     echo json_encode($result);
   } else if($_SERVER['REQUEST_METHOD'] == 'PATCH') {
-    $conn = new PDO("mysql:host=127.0.0.1; dbname=assign1", DB_USER, DB_PASSWORD);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = connect_to_db();
     $sql = "UPDATE Blog SET title = '$title', author = '$author', body = '$body', blogDate = '$date' WHERE id = $id";
     // Prepare statement
     $stmt = $conn->prepare($sql);
@@ -72,9 +76,7 @@
                 $_SERVER['REQUEST_URI']; 
     // This trims the url to leave only the number
     $id  = end(explode('/',trim($link,'/')));
-    $conn = new PDO("mysql:host=localhost; dbname=assign1", DB_USER, DB_PASSWORD);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = connect_to_db();
     $sql = "DELETE FROM Blog WHERE id = $id";
     // Prepare statement
     $conn->exec($sql);
